12c1c85c354ed0237ee962fcf05c0c5a601956c3
Jon Hallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2017-03-24 15:48:17 +0000

    init

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+
+
+  get '/' do
+    File.read("home.html")
+  end
+
+  get '/blog' do
+    File.read('./blog.html')
+  end
+
+  get '/shop' do
+    File.read('./shop.html')
+  end
+
+  get '/quiz' do
+    File.read('./quiz.html')
+  end
